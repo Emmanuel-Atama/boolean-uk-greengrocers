805c3abe01a51553504de8fc5f4a41c3030881cc
Emmanuel Atama <emmanuelatama@outlook.com>
2021-07-27 14:15:12 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,53 @@
+const data = [
+  {
+    id: "001-beetroot",
+    name: "Beetroot",
+    price: 1.4
+  },
+  {
+    id: "002-carrot",
+    name: "Carrot",
+    price: 1.5
+  },
+  {
+    id: "003-apples",
+    name: "Apple",
+    price: 1.65
+  },
+  {
+    id: "004-apricot",
+    name: "Apricot",
+    price: 1.75
+  },
+  {
+    id: "005-avocado",
+    name: "Avocado",
+    price: 1.8
+  },
+  {
+    id: "006-bananas",
+    name: "Bananas",
+    price: 1.45
+  },
+  {
+    id: "007-bell-pepper",
+    name: "Bell-pepper",
+    price: 1.7
+  },
+  {
+    id: "008-berry",
+    name: "Berry",
+    price: 1.6
+  },
+  {
+    id: "009-blueberry",
+    name: "Blueberry",
+    price: 1.95
+  },
+  {
+    id: "010-eggplant",
+    name: "Eggplant",
+    price: 1.75
+  }
+];
+console.log(data);
